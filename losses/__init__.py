0bab99e5bbeedb5d5d23895175a8d5a047ba6f4e
jwon112 <00cjw@naver.com>
2025-11-03 00:02:02 +0900

    .

--- losses/__init__.py
+++ losses/__init__.py
@@ -1,8 +1,10 @@
-from .losses import dice_loss, combined_loss
+from .losses import dice_loss, combined_loss, combined_loss_nnunet_style, soft_dice_loss_with_squared_pred
 
 __all__ = [
     'dice_loss',
     'combined_loss',
+    'combined_loss_nnunet_style',
+    'soft_dice_loss_with_squared_pred',
 ]
 
 
