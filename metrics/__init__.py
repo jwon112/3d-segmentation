4ce132546882c90a8f88886d0522194c85fb746d
jwon112 <00cjw@naver.com>
2025-11-05 18:05:26 +0900

    bug fix

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -1,7 +1,8 @@
-from .metrics import calculate_dice_score
+from .metrics import calculate_dice_score, calculate_wt_tc_et_dice
 
 __all__ = [
     'calculate_dice_score',
+    'calculate_wt_tc_et_dice',
 ]
 
 
